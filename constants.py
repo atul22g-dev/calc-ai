7f9bc4ea2d76b4d1ad2fdf0865ed2e5500412712
Atugatran <atul22gcoders+atugatran@gmail.com>
2024-10-09 00:43:00 +0530

    add prod

--- constants.py
+++ constants.py
@@ -2,8 +2,13 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-SERVER_URL = 'localhost'
-PORT = '8900'
-ENV = 'dev'
+ENV = os.getenv("ENV", "dev")
+
+if ENV == 'production':
+    SERVER_URL = '0.0.0.0'
+    PORT = '8900'
+else:
+    SERVER_URL = 'localhost'
+    PORT = '8900'
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
