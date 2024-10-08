560466ed307da422a2ae652d4429b19095f7f64b
Atugatran <atul22gcoders+atugatran@gmail.com>
2024-10-09 01:18:59 +0530

    add lib

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,16 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import eslint from 'vite-plugin-eslint';
 import { defineConfig } from "vite"
- 
+
 export default defineConfig({
-  plugins: [react(),eslint()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000, // You can specify any port you prefer
   },
 })
